3afb01bcb9825302b3fa3af1b0d532714374528a
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2022-02-11 08:58:42 +0100

    Add files via upload

---
+++ biblioteka.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+  <head>
+
+     <meta charset="utf-8">
+     <title>Biblioteka Miejska</title>
+     <meta name="description" content="Witryna biblioteki miejskiej ">
+     <meta name="keywords" content="Biblioteka Miejska ,ksiązki">
+     <link rel="stylesheet" href="style.css">
+    
+
+  </head>
+  <body>
+
+  </body>
+
+</html>
