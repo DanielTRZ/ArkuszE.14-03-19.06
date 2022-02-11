2d56d070c00f7ad5f36bdc563479832b816a2dac
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2022-02-11 09:13:47 +0100

    Add files via upload

--- biblioteka.php
+++ biblioteka.php
@@ -1,17 +1,60 @@
 <!DOCTYPE html>
 <html lang="pl">
-
   <head>
-
      <meta charset="utf-8">
      <title>Biblioteka Miejska</title>
-     <meta name="description" content="Witryna biblioteki miejskiej ">
-     <meta name="keywords" content="Biblioteka Miejska ,ksiązki">
+     <meta name="description" content="Witryna biblioteki miejskiej w Książkowicach ">
+     <meta name="keywords" content="Biblioteka Miejska ,ksiązki,Książkowice">
      <link rel="stylesheet" href="style.css">
-    
-
   </head>
   <body>
+  <div id="banner">
+      <h2>Miejska Biblioteka Publiczna w Książkowicach</h2>
+      
+  </div>
+  
+  
+  <div id="left">
+     <h3>W naszych zbiorach znajdziesz dzieła następujących autorów:</h3>
+     
+     <?php
+      <ul>
+  <li>element listy</li>
+  <li>element listy</li>
+</ul>
+      ?>
+  </div>
+  
+  
+  <div id="center">
+     <h3>Dodaj nowego czytelnika</h3>
+     <form method="post">
+         imię<input type="text">
+         nazwisko<input type="text">
+         rok urodzenia<input type="number">
+         <input type="button" value="Dodaj">
+         
+         
+         
+     </form>
+      
+  </div>
+  <div id="right">
+      <img src="biblioteka.png" alt="książki">
+      <h4>
+       ul.Czytelnicza 25<br>
+       12-120 Książkowice<br>
+       tel:123123123<br>
+       e-mail: biuro@biblioteka.pl
+      </h4>
+      
+  </div>
+  <div id="footer">
+      
+      <a>Projekt strony:00000000000</a>
+      
+  </div>
+  
 
   </body>
 
